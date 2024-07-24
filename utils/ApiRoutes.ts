b1d9bf5ee651e3392ac38860e3d331e5b71bc21a
Atish Thakur <83630847+atish-tech@users.noreply.github.com>
2024-07-24 16:35:18 +0530

    new commit

--- utils/ApiRoutes.ts
+++ utils/ApiRoutes.ts
@@ -1,6 +1,6 @@
 export const host = "http://13.201.229.100:8000";
 // export const host = "https://test-webscraper.vercel.app"; 
 // export const host = "https://test-webscraper.onrender.com";
-// export const host = "http://43.205.210.46:8000";
+// export const host = "http://43.205.210.46:8000"; 
 
 export const webScraperRoutes = `${host}/api/webscraper`;
