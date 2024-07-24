76111d2f6faf3adc89b7baabe8ffa7d8cd70bbc6
Atish Thakur <83630847+atish-tech@users.noreply.github.com>
2024-07-24 15:04:15 +0530

    Update ApiRoutes.ts

--- utils/ApiRoutes.ts
+++ utils/ApiRoutes.ts
@@ -1,4 +1,4 @@
-export const host = "http://13.235.74.89:8000";
+export const host = "http://13.201.229.100:8000";
 // export const host = "https://test-webscraper.vercel.app";
 // export const host = "https://test-webscraper.onrender.com";
 // export const host = "http://43.205.210.46:8000";
